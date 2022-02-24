96e3c61a5ddcdd2660eae55233ccecf89903e4a3
Golam <74001687+Golam-moula@users.noreply.github.com>
2022-02-24 23:14:14 +0600

    Create d.sh

---
+++ d.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+echo "This is a shell script"  
+nitrogen --restore
+#exec picom
+
+#exec "nitogen --restore"
+#exec picom
+#while true; do
+#	WGET="/usr/bin/wget"
+#
+#	$WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
+#	if [ ! -s /tmp/google.idx ]
+#	then
+#   	xsetroot -name  "Not Connected..!"
+#	else
+#   	xsetroot -name " $(date +%I:%M:%S__%A__%D) "
+#	fi
+#	sleep 1
+#	done
