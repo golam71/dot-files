a9329019d2386d352a22f13822ed4f5cb308ac83
Golam <74001687+Golam-moula@users.noreply.github.com>
2022-03-28 21:56:58 +0600

    Update d.sh

--- d.sh
+++ d.sh
@@ -1,19 +1,21 @@
 #!/bin/bash  
 echo "This is a shell script"  
 nitrogen --restore
-#exec picom
+exec picom 
+#sudo ntpdate time.apple.com
 
-#exec "nitogen --restore"
-#exec picom
-#while true; do
-#	WGET="/usr/bin/wget"
-#
-#	$WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
-#	if [ ! -s /tmp/google.idx ]
-#	then
-#   	xsetroot -name  "Not Connected..!"
-#	else
-#   	xsetroot -name " $(date +%I:%M:%S__%A__%D) "
-#	fi
-#	sleep 1
-#	done
+
+
+while true; do
+	WGET="/usr/bin/wget"
+
+	$WGET -q --tries=20 --timeout=10 http://192.168.31.1 -O /tmp/router.idx &> /dev/null
+	if [ ! -s /tmp/router.idx ]
+	then
+  	xsetroot -name  "Not Connected..!"
+	else
+   	xsetroot -name " $(date +%I:%M:%S__%A__%D) "
+	fi
+	sleep 0 
+	done
+
