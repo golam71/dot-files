880e64ed0c4f38cc9440c69012f3545272a8c7f9
Golam <74001687+golam47@users.noreply.github.com>
2022-07-07 20:29:21 +0600

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,5 @@
-print 
-sudo pacman -Syu
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-yay -Syu
+import os
+print ("full update on the systejm and then installing yay \n")
+os.system("sudo pacman -Syu && sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && yay -Syu")
+print ("I use chaddwm bu siduck its a dwm fork but is cool \n now installing packages that aree needed to install chadwm \n")
+os.system("yay -Sy dash imlib2 xorg-xsetroot ")
